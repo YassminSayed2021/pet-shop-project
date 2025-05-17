4e23ca477c0bdd22f7bf5b52ea873873c8df5033
Yassmin sayed <yassmin.sayed868@gmail.com>
2025-05-17 03:28:11 +0300

    pagination for stored data in firebase store

---
+++ product.js
@@ -0,0 +1,19 @@
+// const products = [
+//     {
+//       id: 1,
+//       title: "Premium Dog Food",
+//       image: "images/product-1.jpg",
+//       hoverImage: "images/product-1_0.jpg",
+//       rating: 4,
+//       price: "$25.99"
+//     },
+//     {
+//       id: 2,
+//       title: "Healthy Cat Treats",
+//       image: "images/product-2.jpg",
+//       hoverImage: "images/product-2_0.jpg",
+//       rating: 5,
+//       price: "$15.49"
+//     },
+//   ];
+  
